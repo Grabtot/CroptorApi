5177d147a68abb79bbd280ef761ef7b20875fed8
Grabtot <dneshotkin@gmail.com>
2024-01-05 02:29:25 +0200

    Added preset events

---
+++ Domain/Presets/Events/PresetEvents.cs
@@ -0,0 +1,8 @@
+﻿using Croptor.Domain.Common.ValueObjects;
+using MediatR;
+
+namespace Croptor.Domain.Presets.Events
+{
+    public record PresetCreated(Preset Preset) : INotification;
+    public record SizeAdded(Preset Preset, Size Size) : INotification;
+}
