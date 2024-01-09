946fa5a1dbe6535eaf61979e681ddbbdf48e3540
ArtCrafter2999 <ArtCrafter2999@gmail.com>
2024-01-09 18:08:06 +0200

    SavePreset

---
+++ Api/ViewModels/Preset/SavePresetDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Croptor.Api.ViewModels.Preset;
+
+public record SavePresetDto(
+    Guid? Id,
+    string Name,
+    List<Domain.Common.ValueObjects.Size> Sizes
+);
---
+++ Application/Presets/Commands/SavePreset/SavePresetCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Croptor.Application.Common.Interfaces.Persistence;
+using Croptor.Domain.Presets;
+using MediatR;
+
+namespace Croptor.Application.Presets.Commands.SavePreset;
+
+public class SavePresetCommandHandler(
+    IPresetRepository presetRepository
+) : IRequestHandler<SavePresetCommand>
+{
+    public async Task Handle(SavePresetCommand request, CancellationToken cancellationToken)
+    {
+        Preset? preset = await presetRepository.GetOrDefaultAsync(request.Preset.Id, cancellationToken);
+        if (preset != null)
+            await presetRepository.UpdateAsync(request.Preset, cancellationToken);
+        else
+            await presetRepository.AddAsync(request.Preset, cancellationToken);
+    }
+}
