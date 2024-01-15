2c706a918122eb3e68c422c6525357e1b765f02f
ArtCrafter2999 <ArtCrafter2999@gmail.com>
2024-01-15 17:21:07 +0200

    Saving and loading files archive

--- Api/Controllers/ImagesController.cs
+++ Api/Controllers/ImagesController.cs
@@ -1,22 +1,32 @@
 ﻿using Croptor.Api.Attributes;
 using Croptor.Api.ViewModels.Image;
 using Croptor.Application.Images.Queries.CropImage;
-using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO.Compression;
 
 namespace Croptor.Api.Controllers;
 
 [Route("images")]
 [ApiController]
-public class ImagesController(/*IMapper mapper,*/ IMediator mediator) : ControllerBase
+public class ImagesController( /*IMapper mapper,*/ IMediator mediator, IHostEnvironment environment) : ControllerBase
 {
     [HttpPost("crop")]
     [MaxFilesAuthorization(3)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    public async Task<ActionResult<string>> Crop([FromForm] List<IFormFile> files, [FromForm] ImagesParamsDto imagesParams)
+    public async Task<ActionResult<string>> Crop(
+        [FromForm] List<IFormFile> files,
+        [FromForm] string images
+    )
     {
+        var imagesParams = JsonConvert.DeserializeObject<ImagesParamsDto>(images);
+        if (imagesParams == null) throw new ArgumentException("images must be a valid json");
+
+        var guid = Guid.NewGuid();
+        var path = Path.Combine(environment.ContentRootPath, "wwwroot", guid.ToString());
+        if (!Directory.Exists(path)) Directory.CreateDirectory(path);
         foreach (var file in files)
         {
             if (!file.ContentType.StartsWith("image/")) continue;
@@ -26,11 +36,27 @@ public class ImagesController(/*IMapper mapper,*/ IMediator mediator) : Controll
             await mediator.Send(new CropImageQuery(
                 memoryStream,
                 file.FileName,
-                imagesParams.Images[file.FileName],
-                imagesParams.Sizes
+                imagesParams.Params[file.FileName],
+                imagesParams.Sizes,
+                path
             ));
         }
 
-        return Ok("Kinda link to archive"); //TODO
+        var newGuid = Guid.NewGuid();
+        var archiveDir = Path.Combine(environment.ContentRootPath, "wwwroot", newGuid.ToString());
+        var archivePath = Path.Combine(archiveDir, "cropped.zip");
+        if (!Directory.Exists(archiveDir)) Directory.CreateDirectory(archiveDir);
+        ZipFile.CreateFromDirectory(path, archivePath);
+        var uri = $"{Request.Scheme}://{Request.Host}/images/download/{newGuid}/cropped.zip";
+        return Created(uri, uri);
+    }
+
+    [HttpGet("download/{id:guid}/cropped.zip")]
+    public async Task<ActionResult<string>> Download(Guid id)
+    {
+        var fileStream =
+            new FileStream(Path.Combine(environment.ContentRootPath, "wwwroot", id.ToString(), "cropped.zip"),
+                FileMode.Open);
+        return File(fileStream, "application/zip");
     }
 }
